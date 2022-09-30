a29a6942f5c3c9e00d102306ebd580ce0d4a0fc1
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2022-09-30 23:49:09 +0100

    Initial import
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,36 @@
 #include <Arduino.h>
+// https://github.com/GreyGnome/EnableInterrupt/wiki/Usage#Fast_Start
 
-#include <PinChangeInt.h>
+#define EI_NOTPORTB
+#define EI_NOTPORTC
+#define EI_NOTEXTERNAL
+#include <EnableInterrupt.h>
+
+//#include <PinChangeInt.h>
  
-#define MY_PIN 5 // we could choose any pin
+#define MY_PIN 5 // PD5  we could choose any pin
  
 volatile int pwm_value = 0;
-volatile int prev_time = 0;
+volatile long prev_time = 0;
 uint8_t latest_interrupted_pin;
- 
-void rising()
-{
-  latest_interrupted_pin=PCintPort::arduinoPin;
-  PCintPort::attachInterrupt(latest_interrupted_pin, &falling, FALLING);
+
+void falling(void);
+
+void rising() {
+  //latest_interrupted_pin=PCintPort::arduinoPin;
+ // PCintPort::attachInterrupt(latest_interrupted_pin, &falling, FALLING);
+  disableInterrupt(MY_PIN);
+  enableInterrupt(MY_PIN,falling,FALLING);
   prev_time = micros();
 }
  
 void falling() {
-  latest_interrupted_pin=PCintPort::arduinoPin;
-  PCintPort::attachInterrupt(latest_interrupted_pin, &rising, RISING);
-  pwm_value = micros()-prev_time;
-  Serial.println(state);
+  //latest_interrupted_pin=PCintPort::arduinoPin;
+  //PCintPort::attachInterrupt(latest_interrupted_pin, &rising, RISING);
+  disableInterrupt(MY_PIN);   enableInterrupt(MY_PIN,rising,RISING);
+  pwm_value = micros() - prev_time;
+
+  //Serial.println(state);
 }
  
 void setup() {
@@ -27,7 +38,8 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
   Serial.println(F("Hello"));
-  PCintPort::attachInterrupt(MY_PIN, &rising, RISING);
+  enableInterrupt(MY_PIN,rising,RISING);
+  //PCintPort::attachInterrupt(MY_PIN, &rising, RISING);
 }
  
 void loop() { 
