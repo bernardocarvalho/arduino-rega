9101f7c3c1b961f141a7a0f219725a71be7ec469
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2022-10-03 23:41:01 +0000

    Added Python logger script
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

---
+++ python/serial-log.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# vim: set ts=4 sw=4 tw=0 et :
+# https://github.com/ikalchev/py-sds011
+########################################################
+__author__ = "Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>"
+__license__ = "GPL3"
+__version__ = "1.0"
+
+import serial
+import time
+import csv, time, datetime
+
+#filename="log-file.txt"
+#datafile=open(filename, 'a')
+
+ser = serial.Serial('/dev/ttyUSB0', baudrate = 115200, parity=serial.PARITY_NONE, \
+        stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
+ser.flushInput()
+
+try:
+    with open("measurments.csv","a") as csvfile:
+# ser_bytes = ser.readline()
+        log = csv.writer(csvfile, delimiter=",",quotechar=" ", quoting=csv.QUOTE_MINIMAL)
+        #logcols = ["timestamp","pm2.5","pm10","device_id"]
+        logcols = ["timestamp       ","sec","Hum 1","Temp 1"," Hum 2","Temp 2","H20 Meas","H2O Pump"]
+        log.writerow(logcols)
+        while True:
+            ser_bytes = ser.readline()
+            str_line = ser_bytes[0:len(ser_bytes)-2].decode("utf-8") # strip \r\n
+            now = datetime.datetime.now()
+        #decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
+            vals = [str(now)] + [str(str_line)] 
+            log.writerow(vals)
+            csvfile.flush()
+            print(vals)
+        #    print(ser_line)
+        #datafile.write(str(time.time()) + str(ser_line))
+    
+except KeyboardInterrupt:
+#        datafile.close()
+        ser.close()
+        print("Keyboard Interrupt")
+        #break
--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,108 @@
 //#include <PinChangeInt.h>
  
 //#define MY_PIN 5 // PD5  we could choose any pin
-#define H_PIN SDA //
+#define H_PIN SDA // Blue wire on sensor
 #define T_PIN SCL //
  
-volatile int pwm_hvalue = 0, pwm_tvalue = 0, cnt_t, cnt_h;
-volatile long prev_htime, prev_ttime, pwm_t, pwm_h;
+#define H2_PIN A3  // PC3
+#define T2_PIN A2  //PC2
 
-uint8_t latest_interrupted_pin;
+#define M_PIN 4 // PD4 
+
+//volatile int pwm_hvalue = 0, pwm_t1value = 0, cnt_t1, cnt_h1;
+volatile long prev_h1time, prev_t1time, pwm_t1, pwm_h1;
+volatile int cnt_t1=0, cnt_h1=0, cnt_m=0;
+
+volatile int cnt_t2, cnt_h2;
+volatile long prev_h2time, prev_t2time, pwm_t2, pwm_h2;
+
+//uint8_t latest_interrupted_pin;
 bool state = false;
-void h_falling(void);
-void t_falling(void);
+// Funtion prototypes
+void h1_falling() {
+  unsigned long us;
+  us = micros();
+  pwm_h1 += us - prev_h1time;
+  prev_h1time = us;
+  cnt_h1++;
+}
+void t1_falling() {
+  unsigned long us;
+  us = micros();
+  pwm_t1 += us - prev_t1time;
+  prev_t1time = us;
+  cnt_t1++;
+}
+void h2_falling() {
+  unsigned long us;
+//  disableInterrupt(H2_PIN); enableInterrupt(H2_PIN, h2_rising, RISING);
+  us = micros();
+  pwm_h2 += us - prev_h2time;
+  prev_h2time = us;
+  cnt_h2++;
+}
+void t2_falling() {
+  unsigned long us;
+  us = micros();
+  pwm_t2 += us - prev_t2time;
+  prev_t2time = us;
+  cnt_t2++;
+}
+void m_falling() {
+  cnt_m++;
+}
 
+void setup() {
+  pinMode(H_PIN, INPUT_PULLUP);
+  pinMode(H2_PIN, INPUT_PULLUP);
+  pinMode(T_PIN, INPUT_PULLUP);
+  pinMode(T2_PIN, INPUT_PULLUP);
+  pinMode(M_PIN, INPUT_PULLUP);
+  // digitalWrite(H_PIN, HIGH);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  Serial.println(F("Hello"));
+  enableInterrupt(H_PIN, h1_falling, FALLING);
+  enableInterrupt(T_PIN, t1_falling, FALLING);
+  enableInterrupt(H2_PIN, h2_falling, FALLING);
+  enableInterrupt(T2_PIN, t2_falling, FALLING);
+  enableInterrupt(M_PIN, m_falling, FALLING);
+  prev_h1time = micros();
+  prev_t1time = prev_h1time;
+}
+ 
+void loop() { 
+    Serial.print(millis()/1000);
+    Serial.print(F(", "));
+    float hval = ((float) pwm_h1 ) / cnt_h1; 
+    pwm_h1 = 0; cnt_h1 = 0;
+    Serial.print(hval);
+    Serial.print(F(", "));    
+    //float tval = ((float) pwm_t1 ); // cnt_t1; 
+    float tval = ((float) pwm_t1 ) / cnt_t1; 
+    pwm_t1 = 0; cnt_t1 = 0;
+    Serial.print(tval);
+    hval = ((float) pwm_h2 ) / cnt_h2; 
+    pwm_h2 = 0; cnt_h2 = 0;
+    Serial.print(F(", "));    
+    Serial.print(hval);
+    tval = ((float) pwm_t2 ) / cnt_t2; 
+    pwm_t2 = 0; cnt_t2 = 0;
+    Serial.print(F(", "));    
+    Serial.print(tval);
+    Serial.print(F(", "));    
+    Serial.println(cnt_m);
+    
+    digitalWrite(LED_BUILTIN, state);
+    state = not state;
+
+    delay(1000);
+}
+//void h_falling(void);
+//void t_falling(void);
+//void h2_falling(void);
+//void t2_falling(void);
+/*
 void h_rising() {
   //latest_interrupted_pin=PCintPort::arduinoPin;
  // PCintPort::at    static bool state;tachInterrupt(latest_interrupted_pin, &falling, FALLING);
@@ -36,43 +127,27 @@ void t_rising() {
   pwm_t += pwm_tvalue;
   cnt_t++;
 }
-
+*/
+/*
 void h_falling() {
   disableInterrupt(H_PIN); enableInterrupt(H_PIN, h_rising, RISING);
   prev_htime = micros();
 }
-
 void t_falling() {
   disableInterrupt(T_PIN); enableInterrupt(T_PIN, t_rising, RISING);
   prev_ttime = micros();
 }
 
-void setup() {
-  pinMode(H_PIN, INPUT_PULLUP);
-  // digitalWrite(H_PIN, HIGH);
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(115200);
-  Serial.println(F("Hello"));
-  enableInterrupt(H_PIN, h_falling, FALLING);
-  enableInterrupt(T_PIN, t_falling, FALLING);
-  prev_htime = micros();
-  prev_ttime = prev_htime;
-  //PCintPort::attachInterrupt(MY_PIN, &rising, RISING);
+void h2_rising() {
+  disableInterrupt(H2_PIN);
+  enableInterrupt(H2_PIN, h2_falling, FALLING);
+  pwm_h2 += micros() - prev_h2time;
+  cnt_h2++;
 }
- 
-void loop() { 
-    //float tval, hval;
-    Serial.print(F("pwm H: "));
-    float hval = ((float) pwm_h ) / cnt_h; 
-    pwm_h = 0; cnt_h = 0;
-    Serial.print(hval);
-    Serial.print(F(", pwm T: "));    
-    float tval = ((float) pwm_t ) / cnt_t; 
-    pwm_t = 0; cnt_t = 0;
-    Serial.println(tval);
-//    Serial.println(pwm_tvalue);
-    digitalWrite(LED_BUILTIN, state);
-    state = not state;
-
-    delay(1000);
+void t2_rising() {
+  disableInterrupt(T2_PIN);
+  enableInterrupt(T2_PIN, t2_falling, FALLING);
+  pwm_t2 += micros() - prev_t2time;
+  cnt_t2++;
 }
+*/
