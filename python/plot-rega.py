a6d5b998bc750cf684773a891a099d5c399b1d68
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2022-10-13 13:21:38 +0100

    Added Sensors RT
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- python/plot-rega.py
+++ python/plot-rega.py
@@ -19,28 +19,41 @@ else:
 df = pd.read_csv(filename, low_memory=False) # 'marks.csv', names=headers)
 x, y = df.Hum_0.min(), df.Hum_0.max()
 # 50.31 51.8
-df['H0S'] = (df.Hum_0 - x) / (y - x)
+#df['H0S'] = (df.Hum_0 - x) / (y - x)
+df['1/H0S'] = 5.0 / (df.Hum_0 - 45.0)
 
 x, y = df.Temp_0.min(), df.Temp_0.max()
 # 569.0 888.12
-df['T0S'] = (df.Temp_0 - x) / (y - x)
+df['1/T0S'] = 300.0 / (df.Temp_0 - 200.0)
 
 x, y = df.Hum_LT.min(), df.Hum_LT.max()
+## 3190.79 7340.12
+# 498.8 775.8
 #df['H2S'] = (df.Hum_2 - x) / (y - x)
-df['HLT'] = df.Hum_LT / 8000.0
+df['HLT'] = df.Hum_LT / 800.0
 x, y = df.Temp_LT.min(), df.Temp_LT.max()
-# 3190.79 7340.12
-
 #df['T2S'] = (df.Temp_2 - x) / (y - x)
-df['TLT'] = 500.0 / df.Temp_LT
-x, y = df.Hum_RB.min(), df.Hum_RB.max()
+df['1/TLT'] = 500.0 / df.Temp_LT
 # 514.23 811.04
-df['HRB'] = 55.0 / df.Hum_RB
-x, y = df.Temp_RB.min(), df.Temp_RB.max()
-# 55.65 110.31
-df['TRB'] = 700.0 / df.Temp_RB
-#752.99 1023.72
 
-df.plot(x='timestamp', y=['H0S', 'T0S', 'HLT','TLT', 'HRB','TRB'])
+x, y = df.Hum_RB.min(), df.Hum_RB.max()
+## 55.65 110.31
+# 2858.07 7252.34
+df['1/HRB'] = 3000.0 / (df.Hum_RB - 0)
+x, y = df.Temp_RB.min(), df.Temp_RB.max()
+#752.99 1023.72
+df['1/TRB'] = 1000.0 / df.Temp_RB
+
+x, y = df.Hum_RT.min(), df.Hum_RT.max()
+# 83299.45  86559.42
+df['HRT'] = (df.Hum_RT - 83000) / 6000.0
+
+
+x, y = df.Temp_RT.min(), df.Temp_RT.max()
+# 634.69 992.98
+df['1/TRT'] = 600.0 / df.Temp_RT
+
+#df.plot(x='timestamp', y=['1/H0S', '1/T0S', 'HLT','1/TLT', '1/HRB','1/TRB'])
+df.plot(x='timestamp', y=['1/H0S', '1/T0S', 'HLT','1/TLT', '1/HRB','1/TRB', 'HRT','1/TRT'])
 plt.show()
 
